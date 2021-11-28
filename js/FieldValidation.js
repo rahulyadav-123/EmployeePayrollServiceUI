ff558751d3d8ff3c7cf55759d57dfe07bf0fc5ee
rahul yadav <ry219613@gmail.com>
2021-11-28 22:42:47 +0530

    set event listner

---
+++ js/FieldValidation.js
@@ -0,0 +1,7 @@
+//applied salary range
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function() {
+    output.textContent = salary.value;
+});
