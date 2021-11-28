7a367b249e98c6d42f9f35795c760ca001ef994f
rahul yadav <ry219613@gmail.com>
2021-11-28 21:48:37 +0530

    reset form

---
+++ js/EmployeePayrollData.js
@@ -0,0 +1,72 @@
+class EmployeePayrollData {
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        let nameRegex = RegExp("^[A-Z]{1}[a-zA-Z\\s]{2,}$");
+        if (nameRegex.test(name))
+            this._name = name;
+        else throw "Name is incorrect";
+    }
+
+    get profilePic() {
+        return this._profilePic;
+    }
+
+    set profilePic(profilePic) {
+        this._profilePic = profilePic;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get department() {
+        return this._department;
+    }
+
+    set department(department) {
+        this._department = department;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+        this._salary = salary;
+    }
+
+    get note() {
+        return this._note;
+    }
+
+    set note(note) {
+        this._note = note;
+    }
+
+    get start_date() {
+        return this._start_date;
+    }
+
+    set start_date(start_date) {
+        let now = new Date();
+        now = Date.parse(now);
+        if (start_date > now) {
+            throw 'Start Date is Future date!';
+        } else {
+            this._start_date = start_date;
+        }
+    }
+
+    toString() {
+        return 'Name = ' + this.name + ", Gender = " + this.gender + ", ProfilePic = " + this.profilePic +
+            ", Department = " + this.department + ", Salary = " + this.salary +
+            ", StartDate = " + this.start_date + ", Note = " + this.note;
+    }
+}
