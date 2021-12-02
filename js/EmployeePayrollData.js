07db281bfb9eeda7099e461c2f4ea5eee5eceedb
rahul yadav <ry219613@gmail.com>
2021-12-02 21:25:54 +0530

    reset form using java script

--- js/EmployeePayrollData.js
+++ js/EmployeePayrollData.js
@@ -58,10 +58,19 @@ class EmployeePayrollData {
         this._start_date = start_date;
     }
 
+    set start_date(start_date) {
+        let now = new Date();
+        now = Date.parse(now);
+        if (start_date > now) {
+            throw 'Start Date is Future date!';
+        } else {
+            this._start_date = start_date;
+        }
+    }
+
     toString() {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const empDate = !this.start_date ? "undefined" : this.start_date.toLocaleDateString("en-US", options);
         return 'Name = ' + this.name + ", Gender = " + this.gender + ", ProfilePic = " + this.profilePic +
-            ", Department = " + this.department + ", Salary = " + this.salary + ", StartDate = " + empDate + ", Note = " + this.note;
+            ", Department = " + this.department + ", Salary = " + this.salary +
+            ", StartDate = " + this.start_date + ", Note = " + this.note;
     }
 }
